ddb7a3470c0ac16cabbefb6bc440595634e1e075
Ryan <r.bartelds@student.utwente.nl>
2026-05-29 13:21:12 +0200

    memory efficiency

--- cache.go
+++ cache.go
@@ -8,25 +8,26 @@ import (
 
 type Cache struct {
 	data  map[string][]dns.RR
-	order []string
+	order []*string
 	mutex sync.Mutex
 
 	maximum_size int
 }
 
 func newCache(maximum_size int) *Cache {
-	return &Cache{data: make(map[string][]dns.RR), order: make([]string, 0), maximum_size: maximum_size}
+	return &Cache{data: make(map[string][]dns.RR), order: make([]*string, 0), maximum_size: maximum_size}
 }
 
 func (c *Cache) Put(rr dns.RR, rrs *[]dns.RR) {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
 	if len(c.order) == c.maximum_size {
-		delete(c.data, c.order[0])
+		delete(c.data, *c.order[0])
 		c.order = c.order[1:]
 	}
-	c.data[rr.String()] = *rrs
-	c.order = append(c.order, rr.String())
+	str := rr.String()
+	c.data[str] = *rrs
+	c.order = append(c.order, &str)
 }
 
 func (c *Cache) Get(rr dns.RR) (*[]dns.RR, bool) {
