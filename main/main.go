d53f1ebc250f903751d909f1c709b934220a4f63
ryan0 <r.bartelds@student.utwente.nl>
2026-06-02 15:35:53 +0200

    added ip, port, cache limit and queue limit as command line argument

--- main/main.go
+++ main/main.go
@@ -1,22 +1,53 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net"
+	"net/netip"
+	"os"
+	"strconv"
+
 	ns "github.com/Ryanbar20/OpenINTEL_DNS_NAME_SERVER"
 )
 
 func main() {
+	if len(os.Args) != 4 {
+		fmt.Printf("Usage: %s <cache_limit> <queue_limit> <ip:port>\n", os.Args[0])
+		os.Exit(1)
+	}
 
-	nameserver := ns.NewNameServer(10, 10, "[::]", ns.NAME_SERVER_PORT)
+	cache_limit, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal("cache limit (first argument) must be an integer")
+	}
+
+	queue_limit, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatal("queue limit (second argument) must be an integer")
+	}
+
+	addr := os.Args[3]
+	var host string
+	var portStr string
+	if host, portStr, err = net.SplitHostPort(addr); err != nil {
+		log.Fatal("invalid address")
+	}
+
+	ip, err := netip.ParseAddr(host)
+	if err != nil {
+		log.Fatal("invalid ip")
+	}
+
+	if ip.Is6() {
+		host = "[" + host + "]" // brackets are needed for miekgdns
+	}
+
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		log.Fatal("port must be an integer")
+	}
+
+	nameserver := ns.NewNameServer(cache_limit, queue_limit, host, port)
 	nameserver.Start()
-	// dns.HandleFunc(dom, handle_query)
-
-	// go query_thread()
-	// go serve("tcp")
-	// go serve("udp")
-
-	// sig := make(chan os.Signal, 1)
-	// signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-	// s := <-sig
-	// fmt.Printf("Signal (%s) received, stopping", s)
-
 }
